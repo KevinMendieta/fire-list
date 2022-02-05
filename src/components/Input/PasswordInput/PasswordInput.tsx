0fff512be9135e2a2b3126908d925953317f850f
KevinMendieta <kevin@wonderschool.com>
2022-02-05 00:21:46 -0500

    move password input component

---
+++ src/components/Input/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FieldValues, UseFormRegister } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import {
+  Button,
+  Icon,
+  Input,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+
+interface PasswordInputProps {
+  register: UseFormRegister<FieldValues>;
+}
+
+export default function PasswordInput(props: PasswordInputProps) {
+  const { register } = props;
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  return (
+    <>
+      <FormLabel htmlFor="password">Password</FormLabel>
+      <InputGroup>
+        <Input
+          id="password"
+          type={showPassword ? "text" : "password"}
+          backgroundColor="white"
+          {...register("password", { required: true })}
+        />
+        <InputRightElement width="4rem">
+          <Button
+            h="1.75rem"
+            size="sm"
+            bgColor="#0c2d48"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            <Icon color="white" as={showPassword ? FaEye : FaEyeSlash} />
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </>
+  );
+}
