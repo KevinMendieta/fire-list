21528956a87cf2fb23e104dba810b1aa3952ae35
KevinMendieta <kevin@wonderschool.com>
2022-02-05 22:07:56 -0500

    added basic home component

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Center, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Box>
+      <Center>
+        <Box w={500} p={4}>
+          <Text as="h2">Hey There! 🔥</Text>
+        </Box>
+      </Center>
+    </Box>
+  );
+}
