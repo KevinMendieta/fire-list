c9b7903b634f6194312ee3accf50ba3df32d4ecb
KevinMendieta <kevin@wonderschool.com>
2022-02-05 22:14:25 -0500

    added home route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,12 +4,15 @@ import { Router, Switch, Route } from "wouter";
 const LoginView = React.lazy(() => import("../views/Login"));
 const SignupView = React.lazy(() => import("../views/Signup"));
 
+const HomeView = React.lazy(() => import("../views/Home"));
+
 export default function AppRouter() {
   return (
     <Router>
       <Switch>
         <Route path="/login" component={LoginView} />
         <Route path="/signup" component={SignupView} />
+        <Route path="/home" component={HomeView} />
       </Switch>
     </Router>
   );
