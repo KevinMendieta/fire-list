742b3d4cc64e768d411463510ce83ecb19311a14
KevinMendieta <kevin@wonderschool.com>
2022-02-08 22:57:06 -0500

    add forgot password route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -3,9 +3,12 @@ import { Router, Switch } from "wouter";
 
 import { PrivateRoute, PublicRoute } from ".";
 
+// Public views
 const LoginView = React.lazy(() => import("../views/Login"));
 const SignupView = React.lazy(() => import("../views/Signup"));
+const ForgotPasswordView = React.lazy(() => import("../views/ForgotPassword"));
 
+// Private views
 const HomeView = React.lazy(() => import("../views/Home"));
 
 export default function AppRouter() {
@@ -14,6 +17,7 @@ export default function AppRouter() {
       <Switch>
         <PublicRoute path="/login" component={LoginView} />
         <PublicRoute path="/signup" component={SignupView} />
+        <PublicRoute path="/forgot-password" component={ForgotPasswordView} />
         <PrivateRoute path="/home" component={HomeView} />
       </Switch>
     </Router>
