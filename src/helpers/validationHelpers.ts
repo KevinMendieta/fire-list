e3a98e7b0d13701bf83e37ee183eca05e3650b9b
KevinMendieta <kevin@wonderschool.com>
2022-02-22 22:07:21 -0500

    add password validation function

--- src/helpers/validationHelpers.ts
+++ src/helpers/validationHelpers.ts
@@ -1,6 +1,18 @@
+import v8n from "v8n";
+
 const EMAIL_PATTERN =
   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
 export function validateEmail(email: string): boolean {
   return EMAIL_PATTERN.test(email);
+}
+
+export function validatePassword(password: string): boolean {
+  return v8n()
+    .string()
+    .minLength(8)
+    .some.uppercase()
+    .some.lowercase()
+    .some.numeric()
+    .some.test(password);
 }
