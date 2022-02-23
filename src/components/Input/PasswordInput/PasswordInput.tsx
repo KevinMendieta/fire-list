ff3fbfa9e74f50e93af5ec32a07187af91bd8537
KevinMendieta <kevin@wonderschool.com>
2022-02-22 22:08:43 -0500

    add validations to password input component

--- src/components/Input/PasswordInput/PasswordInput.tsx
+++ src/components/Input/PasswordInput/PasswordInput.tsx
@@ -1,23 +1,47 @@
 import React from "react";
-import { FieldValues, UseFormRegister } from "react-hook-form";
+import {
+  FieldValues,
+  FormState,
+  UseFormRegister,
+  RegisterOptions,
+} from "react-hook-form";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import {
   Button,
   Icon,
   Input,
   FormLabel,
+  FormHelperText,
   InputGroup,
   InputRightElement,
 } from "@chakra-ui/react";
 
+import { validatePassword } from "../../../helpers/validationHelpers";
+
 interface PasswordInputProps {
   register: UseFormRegister<FieldValues>;
+  formState: FormState<FieldValues>;
+  checkFormat?: boolean;
 }
 
 export default function PasswordInput(props: PasswordInputProps) {
-  const { register } = props;
+  const {
+    register,
+    formState: { errors },
+    checkFormat = false,
+  } = props;
   const [showPassword, setShowPassword] = React.useState(false);
 
+  const getValidations = (): RegisterOptions => {
+    const validations: RegisterOptions = {
+      required: true,
+    };
+    if (checkFormat) {
+      validations.validate = (value) => validatePassword(value);
+    }
+    return validations;
+  };
+  console.log(errors);
   return (
     <>
       <FormLabel htmlFor="password">Password</FormLabel>
@@ -27,7 +51,7 @@ export default function PasswordInput(props: PasswordInputProps) {
           type={showPassword ? "text" : "password"}
           backgroundColor="white"
           placeholder="Enter your password"
-          {...register("password", { required: true })}
+          {...register("password", getValidations())}
         />
         <InputRightElement width="4rem">
           <Button
@@ -40,6 +64,13 @@ export default function PasswordInput(props: PasswordInputProps) {
           </Button>
         </InputRightElement>
       </InputGroup>
+      {errors.password && (
+        <FormHelperText color="red">
+          {errors.password.type === "required"
+            ? "Email is required."
+            : "Password must be at least 8 characters long and contain at least 1 uppercase, lowercase and numeric character."}
+        </FormHelperText>
+      )}
     </>
   );
 }
